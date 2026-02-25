7ef4ff9cbf64f56306a7fd51108b3e82c6c90960
MichealProject <anonim213g6@gmail.com>
2026-02-25 10:10:14 +0100

    Dodaje przykładowy wygląd strony głównej

---
+++ pages/front/Fhome.php
@@ -0,0 +1,7 @@
+<div class="row g-0 mx-0">
+    <!-- Główna zawartość strony -->
+    <div class='d-flex flex-column col-12 col-lg-10 offset-0 offset-lg-2 p-5 pb-2'>
+        Miłej zabawy życze! :) <br>
+        Zapraszam do zapoznania się z ofertą książek, które mamy w naszej
+    </div>
+</div>
