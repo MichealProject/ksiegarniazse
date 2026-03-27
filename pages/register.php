a0ba1831de3ea7b57b62775725a880b7974d7a10
Kokonos <kokosowy12365@gmail.com>
2026-03-27 01:23:50 +0100

    Update register.php

--- pages/register.php
+++ pages/register.php
@@ -1,20 +1,19 @@
 <?php
-
-$conn = new mysqli('localhost','root','','bookdb');
-if($conn->connect_errno)
-    die("Błąd połączenia".$conn->connect_error);
-
 if($_SERVER['REQUEST_METHOD']=='POST'){
     $email=$_POST["email"];
     $pass=password_hash(trim($_POST["pass"],''),PASSWORD_DEFAULT);
     $fName=$_POST["fName"];
     $lName=$_POST["lName"];
     $sql="INSERT INTO customers(email,password_hash,name,surname) VALUES (?,?,?,?)";
-    $stmt=$conn->prepare($sql);
+    $stmt=$mysql->prepare($sql);
     $stmt->bind_param('ssss',$email,$pass,$fName,$lName);
     $stmt->execute();
-    header("location:flogin.php");
     $stmt->close();
-    $conn->close();
+    header("location:?page=userLogin");
 }
+
+ob_start();
+include 'front/Fregister.php';
+$content = ob_get_clean();
+$display->toDisplay($content);
 ?>
