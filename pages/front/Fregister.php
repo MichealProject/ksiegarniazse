595a0bbc6a21a8340b8c5cdf22a3cd3a8c3976b1
Kokonos <kokosowy12365@gmail.com>
2026-03-27 01:25:32 +0100

    Update Fregister.php

--- pages/front/Fregister.php
+++ pages/front/Fregister.php
@@ -1,48 +1,38 @@
-<?php
-require __DIR__ . '/../register.php';
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rejestracja</title>
-    <style>
-        #info{
-            display:none;
-        }
-        #warning,#warning2{
-            display: none;
-            color:red;
-        }
-        .invalid{
-            color: red;
-        }
-        .valid{
-            color: green;
-        }
-        #sPage{
-            display: none;
-        }
-        #button2,#sButton{
-            display: none;
-        }
-    </style>
-</head>
-<body>
-<main>
-    <form action="fregister.php" method="post">
-        <h1>Utwórz konto</h1>
+<style>
+    #info{
+        display:none;
+    }
+    #warning,#warning2{
+        display: none;
+        color:red;
+    }
+    .invalid{
+        color: red;
+    }
+    .valid{
+        color: green;
+    }
+    #sPage{
+        display: none;
+    }
+    #button2,#sButton{
+        display: none;
+    }
+</style>
+<main class="col-11 col-lg-7 form-signin bg-body-tertiary rounded m-auto">
+    <form action="?page=register" method="post">
+        <h1 class="h3 mb-2 fw-normal">Witamy w Ksiegarni ZSE</h1>
+        <p class="text-secondary mb-3">Zarejestruj się.</p>
         <!-- Pierwsza strona formularza -->
         <div id="fPage">
-            <div>
+            <div class="form-floating">
+                <input type="email" name="email" id="email" pattern="^[^\s@]+@[^\s@]+\.[^\s@]{2,}$" class="form-control">
                 <label for="email">Adres e-mail:</label>
-                <input type="email" name="email" id="email" pattern="^[^\s@]+@[^\s@]+\.[^\s@]{2,}$">
                 <p id="warning"></p> <!-- Powiadomienie o błędach przy wprowadzaniu maila -->
             </div>
-            <div>
+            <div class="form-floating position-relative">
+                <input type="password" name="pass" id="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}" class="form-control" onfocus="showInfo()" onblur="hideInfo()" onkeyup="checkIfValid()">
                 <label for="password">Hasło:</label>
-                <input type="password" name="pass" id="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}">
             </div>
             <!-- Dynamiczna informacja o wymaganiach hasła -->
             <div id="info">
@@ -52,9 +42,9 @@ require __DIR__ . '/../register.php';
                 <p id="upperCase" class="invalid">Wielką literę</p>
                 <p id="lowerCase" class="invalid">Małą literę</p>
             </div>
-            <div>
+            <div class="form-floating">
+                <input type="password" name="pass2" id="password2" class="form-control">
                 <label for="password2">Powtórz hasło:</label>
-                <input type="password" name="pass2" id="password2">
                 <p id="warning2"></p> <!-- Błędy związane z hasłami -->
             </div>
         </div>
@@ -62,24 +52,20 @@ require __DIR__ . '/../register.php';
         <div id="sPage">
             <div>
                 <label for="fName">Imię:</label>
-                <input type="text" name="fName" id="fName" onkeyup="submitCheck()">
+                <input type="text" name="fName" id="fName" onkeyup="submitCheck()" class="form-control">
             </div>
             <div>
                 <label for="lName">Nazwisko:</label>
-                <input type="text" name="lName" id="lName" onkeyup="submitCheck()">
+                <input type="text" name="lName" id="lName" onkeyup="submitCheck()" class="form-control">
             </div>
             <div>
                 <label for="TOS">Akceptuję <a href="tos.html">warunki korzystania ze strony</a></label>
-                <input type="checkbox" name="TOS" id="TOS" onchange="submitCheck()">
+                <input type="checkbox" name="TOS" id="TOS" onchange="submitCheck()" target="_blank">
             </div>
         </div>
-        <button type="button" id="button" onclick="nextCheck()">Dalej</button> <!-- Przycisk do przejścia na drugą stronę -->
-        <button type="button" id="button2" onclick="back()">Wróć</button> <!-- Przycisk powrotu do pierwszej strony -->
-        <button id="sButton" type="submit" disabled="true">Utwórz konto</button>
+        <button type="button" id="button" onclick="nextCheck()" class="btn btn-primary">Dalej</button> <!-- Przycisk do przejścia na drugą stronę -->
+        <button type="button" id="button2" onclick="back()" class="btn btn-primary">Wróć</button> <!-- Przycisk powrotu do pierwszej strony -->
+        <button id="sButton" type="submit" disabled="true" class="btn btn-primary">Utwórz konto</button>
         <footer>Masz już konto? <a href="Flogin.php">Zaloguj się</a></footer>
     </form>
 </main>
-    <script src="../../components/js/validation.js">
-    </script>
-</body>
-</html>
